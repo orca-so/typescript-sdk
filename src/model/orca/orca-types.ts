591928400d610f7dd23a25af8d524e59a03d887e
meep <33380758+odcheung@users.noreply.github.com>
2021-07-14 15:56:08 +0800

    Adding new pools for MVP (#8)
    - Adding all MVP supported pools in the config
    - Adding documentation for the orca-types
    - Removing duplicated file in models/pools
    - Adding id field to OrcaToken

--- src/model/orca/orca-types.ts
+++ src/model/orca/orca-types.ts
@@ -6,18 +6,18 @@ import { Percentage } from "../utils/percentage";
  * @param address The pool account address
  * @param nonce The nonce used to generate the pool authority
  * @param authority The pool authority PDA address to sign instructions
- * @param poolTokenAddress Public address for the pool token
  * @param poolTokenMint Mint address for the pool token
  * @param poolTokenDecimals Number of decimal places for the pool token
  * @param feeAccount Public address of the pool token fee account
- * @param tokens The name, public key pair of the tokens in this pool
+ * @param tokenIds The ids of the tokens in this pool
+ * @param tokens The id, token object of the tokens in this pool
  * @param curveType Trading curve type. 0 - ConstantProduct, 1 - ConstantPrice, 3 - Offset
+ * @param feeStructure The % of fees collected by this pool
  */
 export type OrcaPoolParams = {
   address: PublicKey;
   nonce: number;
   authority: PublicKey;
-  poolTokenAddress: PublicKey;
   poolTokenMint: PublicKey;
   poolTokenDecimals: number;
   feeAccount: PublicKey;
@@ -27,12 +27,24 @@ export type OrcaPoolParams = {
   feeStructure: FeeStructure;
 };
 
+/**
+ * An Orca Token
+ * @param id The id of the token
+ * @param name The presentable name of the token
+ * @param mint The mint public key for the token
+ * @param decimals The number of integers in the u64 return type representing the fractional part of the number
+ */
 export type OrcaToken = {
+  id: string;
   name: string;
   mint: PublicKey;
   decimals: number;
 };
 
+/**
+ * An Orca Token within an OrcaPool
+ * @param addr The public key for this token for this Orca Pool
+ */
 export type OrcaPoolToken = OrcaToken & {
   addr: PublicKey;
 };
@@ -44,6 +56,10 @@ export enum CurveType {
   Offset,
 }
 
+/**
+ * @param traderFee Percentage of the fee that goes to the liquidity providers
+ * @param ownerFee Percentage of the fee that goes to orca
+ */
 export type FeeStructure = {
   traderFee: Percentage;
   ownerFee: Percentage;
--- src/public/pools.ts
+++ src/public/pools.ts
@@ -6,6 +6,11 @@
 /**
  * A list of supported Orca pools in this SDK.
  */
+
 export enum OrcaPoolConfig {
+  SOL_USDC = "SOL_USDC",
+  SOL_USDT = "SOL_USDT",
+  RAY_SOL = "RAY_SOL",
+  ETH_SOL = "ETH_SOL",
   ETH_USDC = "ETH_USDC",
 }
