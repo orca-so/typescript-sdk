cc261d86721e723fd2ec0619636494f84c83aff7
Otto Cheung <otto@orca.so>
2021-08-10 17:33:56 +0800

    Create ATA account instruction in swap() if it does not exist
    - If the user's wallet does not have the AT address for the input/output
      token, append the instruction to initialize it.

--- src/public/utils/models/instruction.ts
+++ src/public/utils/models/instruction.ts
@@ -6,6 +6,12 @@ import {
   TransactionSignature,
 } from "@solana/web3.js";
 
+export const emptyInstruction: Instruction = {
+  instructions: [],
+  cleanupInstructions: [],
+  signers: [],
+};
+
 export type Instruction = {
   instructions: TransactionInstruction[];
   cleanupInstructions: TransactionInstruction[];
--- src/public/utils/web3/instructions/token-instructions.ts
+++ src/public/utils/web3/instructions/token-instructions.ts
@@ -1,5 +1,12 @@
 import { AccountLayout, Token, TOKEN_PROGRAM_ID, u64 } from "@solana/spl-token";
-import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+import {
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  SYSVAR_RENT_PUBKEY,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { Instruction, SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID } from "../..";
 import { ResolvedTokenAddressInstruction } from "../ata-utils";
 
 export const createWSOLAccountInstructions = (
@@ -40,3 +47,60 @@ export const createWSOLAccountInstructions = (
     signers: [tempAccount],
   };
 };
+
+export function createAssociatedTokenAccountInstruction(
+  associatedTokenAddress: PublicKey,
+  fundSource: PublicKey,
+  destination: PublicKey,
+  tokenMint: PublicKey,
+  fundAddressOwner: Keypair
+): Instruction {
+  const systemProgramId = new PublicKey("11111111111111111111111111111111");
+  const keys = [
+    {
+      pubkey: fundSource,
+      isSigner: true,
+      isWritable: true,
+    },
+    {
+      pubkey: associatedTokenAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: destination,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: tokenMint,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: systemProgramId,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: TOKEN_PROGRAM_ID,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: SYSVAR_RENT_PUBKEY,
+      isSigner: false,
+      isWritable: false,
+    },
+  ];
+  const createATAInstruction = new TransactionInstruction({
+    keys,
+    programId: SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
+    data: Buffer.from([]),
+  });
+  return {
+    instructions: [createATAInstruction],
+    cleanupInstructions: [],
+    signers: [fundAddressOwner],
+  };
+}
