56861a1624cf4d5910023074bb8853b9b5d43043
Otto Cheung <otto@orca.so>
2021-08-18 08:16:25 +0800

    update orca swap

--- src/public/utils/constants.ts
+++ src/public/utils/constants.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const ORCA_TOKEN_SWAP_ID: PublicKey = new PublicKey(
-  "DjVE6JNiYqPL2QXyCUUh8rNjHrbz9hXHNYt99MQ59qw1"
+  "9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP"
 );
 
 export const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID: PublicKey = new PublicKey(
