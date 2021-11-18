cff578627221fdd921e06034ccb69230c05c5a7b
0xscuba <88916581+0xscuba@users.noreply.github.com>
2021-11-17 16:48:57 -0800

    Add devnet

--- src/model/orca/orca-impl.ts
+++ src/model/orca/orca-impl.ts
@@ -1,21 +1,23 @@
 import { Connection } from "@solana/web3.js";
-import { Orca, OrcaFarm, OrcaPool, OrcaPoolConfig, OrcaFarmConfig } from "../../public";
+import { Orca, OrcaFarm, OrcaPool, OrcaPoolConfig, OrcaFarmConfig, Network } from "../../public";
 import { OrcaFactory } from "../orca-factory";
 
 export class OrcaImpl implements Orca {
   private connection: Connection;
+  private network: Network;
   private factory: OrcaFactory;
 
-  constructor(connection: Connection) {
+  constructor(connection: Connection, network: Network) {
     this.connection = connection;
+    this.network = network;
     this.factory = new OrcaFactory();
   }
 
   getPool(pool: OrcaPoolConfig): OrcaPool {
-    return this.factory.getPool(this.connection, pool);
+    return this.factory.getPool(this.connection, this.network, pool);
   }
 
   getFarm(farm: OrcaFarmConfig): OrcaFarm {
-    return this.factory.getFarm(this.connection, farm);
+    return this.factory.getFarm(this.connection, this.network, farm);
   }
 }
